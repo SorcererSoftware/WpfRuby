8edecb51ede0a361865c5b4c2f8e60f209970794
SorcererSoftware <SorcererSoftware@gmail.com>
2015-02-20 18:41:08 -0600

    Initial Push for WpfRuby
    Boilerplate Wpf stuff - no actual code yet.

---
+++ WpfRuby/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WpfRuby")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sorcerer Software")]
+[assembly: AssemblyProduct("WpfRuby")]
+[assembly: AssemblyCopyright("Copyright © Sorcerer Software 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
